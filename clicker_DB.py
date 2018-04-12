18deed2227dcdbcf61a792bfdbbc906970728bba
Tyler Behunin <s.tyler.behunin@gmail.com>
2018-04-11 19:10:41 -0600

    database updates

--- clicker_DB.py
+++ clicker_DB.py
@@ -7,17 +7,17 @@ import os
 
 class adminDB:
 	def __init__(self):
-		urllib.parse.uses_netloc.append("postgres")
-        url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
-        self.connection = psycopg2.connect(
-            cursor_factory = psycopg2.extras.RealDictCursor,
-            database = url.path[1:],
-            user = url.username,
-            password = url.password,
-            host = url.hostname,
-            port = url.port
-        )
-        self.cursor = self.connection.cursor()
+		urllib.parse.uses_netloc.append("postgress")
+		url = urllib.parse.urlpars(os.environ["DATABASE_URL"])
+		self.connection = psycopg2.connect(
+			cursor_factory = psycopg2.extras.RealDictCursor,
+			database = url.path[1:],
+			user = url.username,
+			password = url.password,
+			host = url.hostname,
+			port = url.port
+		)
+		self.cursor = self.connection.cursor()
 
 	def __del__(self):
 		self.connection.close()
@@ -60,17 +60,17 @@ class adminDB:
 
 class questionDB:
 	def __init__(self):
-		urllib.parse.uses_netloc.append("postgres")
-        url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
-        self.connection = psycopg2.connect(
-            cursor_factory = psycopg2.extras.RealDictCursor,
-            database = url.path[1:],
-            user = url.username,
-            password = url.password,
-            host = url.hostname,
-            port = url.port
-        )
-        self.cursor = self.connection.cursor()
+		urllib.parse.uses_netloc.append("postgress")
+		url = urllib.parse.urlpars(os.environ["DATABASE_URL"])
+		self.connection = psycopg2.connect(
+			cursor_factory = psycopg2.extras.RealDictCursor,
+			database = url.path[1:],
+			user = url.username,
+			password = url.password,
+			host = url.hostname,
+			port = url.port
+		)
+		self.cursor = self.connection.cursor()
 
 
 
